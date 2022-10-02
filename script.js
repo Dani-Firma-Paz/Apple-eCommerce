3152e36034d594255c8addb44c43468089be49ca
Daniela Firma Paz <danii.firma.paz@gmail.com>
2022-10-01 22:37:22 -0300

    Second commit

---
+++ script.js
@@ -0,0 +1,40 @@
+/*SlideShow*/
+const slideshowDivs = () => {
+    for(let i = 1; i <= 5; i++) {
+        const div = document.createElement('div')
+
+        div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`
+
+        i === 1 && div.classList.add('change')
+
+        document.querySelector('.slideshow').appendChild(div)
+    }
+}
+slideshowDivs();
+
+const divs = document.querySelectorAll('.slideshow div')
+
+/*Para que el contador vuelva a empezar de cero y las imagenes se vuelvan a repetir una y otra vez sin parar 
+debemos crear esto*/
+let a = 1
+
+const slideshow = () => {
+    setInterval(() => {
+        a++
+
+        const div = document.querySelector('.slideshow .change')
+
+        div.classList.remove('change')
+
+        if(a < divs.length) {
+            div.nextElementSibling.classList.add('change')
+        }else{
+            divs[0].classList.add('change')
+            a = 1
+        }
+
+        
+    }, 20000);
+}
+slideshow();
+/*End SlideShow*/
